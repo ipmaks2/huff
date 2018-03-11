9537bc099bbdf80ad1d5290165b2c20bffcf1865
Maks S <ipmaks2@mail.ru>
2018-03-12 00:44:21 +0300

    Updates in python main

--- python/huff_main.py
+++ python/huff_main.py
@@ -1,2 +1,31 @@
-import huff_tools
+from huff_tools import MAXBITS
 import huff_codes
+
+freqs = dict()
+buff = list()    
+tupes = list()
+
+f = open("data.1", "rb")
+try:
+    byte = f.read(1)
+    while byte != "":
+        # Do stuff with byte.
+        buff.append(byte)
+        if freqs.has_key(byte):
+            freqs[byte] += 1
+        else:
+            freqs[byte] = 1
+            
+        byte = f.read(1)
+finally:
+    f.close()
+
+tups = freqs.items()
+huff_table = huff_codes.getCodes(tups)  
+
+
+print freqs
+print buff
+print tups
+print huff_table
+
